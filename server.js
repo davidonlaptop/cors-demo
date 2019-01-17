4e9aaaf613058173e382eefd9ee2baa5bfd745b4
David Lauzon <davidonlaptop@gmail.com>
2019-01-17 17:43:16 -0500

    adding initial work§

---
+++ server.js
@@ -0,0 +1,63 @@
+// Usage: node server1.js
+
+var express = require('express');
+
+
+/////////////////////////
+// API APP
+/////////////////////////
+
+var API_PORT = 9999;
+
+var POSTS = {
+  "1": {'post': "First blog post"},
+  "2": {'post': "Second blog post"},
+  "3": {'post': "Third post"}
+};
+
+var apiApp = express();
+
+
+apiApp.all("/*", function(req, res, next) {
+  console.log("\n>> " + req.method + " http://" + req.headers.host + req.url);
+  console.log( req.headers);
+  next();
+  console.log("<< ", res.statusCode + " " + res.statusMessage);
+  console.log(JSON.parse(JSON.stringify(res.getHeaders())));
+});
+
+
+var corsFilter = function(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+};
+
+// Un/comment this line to enable/disable CORS support
+apiApp.use(corsFilter);
+
+apiApp.use(express.static(__dirname));	// Serve static files from the same directory as this file
+
+apiApp.get("/api/posts", function(req, res) {
+  res.json(POSTS);
+});
+
+apiApp.listen(API_PORT, function() {
+  console.log("Started API server at http://localhost:" + API_PORT);
+});
+
+
+
+/////////////////////////
+// CLIENT APP
+/////////////////////////
+
+var CLIENT_PORT = 1111;
+
+var clientApp = express();
+clientApp.use(express.static(__dirname));       // Serve static files from the same directory as this file
+clientApp.listen(CLIENT_PORT, function() {
+  console.log("Started server at http://localhost:" + CLIENT_PORT);
+});
+
+
+
